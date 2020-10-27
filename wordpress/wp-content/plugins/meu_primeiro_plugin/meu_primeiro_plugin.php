5c8ea41c673bb5523d94a51a25020aa9c27ae584
Bruno Pontes <53492661+brunopontes90@users.noreply.github.com>
2020-10-27 20:48:16 -0300

    criado puglin colocar tags

--- wordpress/wp-content/plugins/meu_primeiro_plugin/meu_primeiro_plugin.php
+++ wordpress/wp-content/plugins/meu_primeiro_plugin/meu_primeiro_plugin.php
@@ -10,6 +10,7 @@
  * License: CC BY
  */
 
+ //login_errors é o gancho
  add_filter('login_errors', 'altera_msg_login');
 
  function altera_msg_login(){
@@ -17,3 +18,25 @@
      return 'Credenciais inválidas';
 
  }
+
+ add_action('wp_head', 'colocar_og_tags');
+     function colocar_og_tags(){
+        if(is_single()){
+
+            $dados_do_post = get_post(get_the_ID());
+               
+            $nome_do_site = get_bloginfo();
+
+            $titulo = $dados_do_post -> post_title;
+
+            $resumo = $dados_do_post -> post_excerpt;
+
+            echo "\n\n\n
+                <meta property='og:title' content='". the_title() . "'>\n
+                <meta property='og:site_name' content='". $nome_do_site . "'>\n
+                <meta property='og:url' content='". get_permalink() . "'>\n
+                <meta property='og:description' content='". get_the_ID()    . "'>\n
+                \n\n\n";
+        }
+     }
+ 
