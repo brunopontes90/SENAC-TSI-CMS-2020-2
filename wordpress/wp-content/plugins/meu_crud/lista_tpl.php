04b91bf4d9921d68e50591080b135fd392fe4e27
Bruno Pontes <53492661+brunopontes90@users.noreply.github.com>
2020-11-24 20:44:42 -0300

    atualização da aula-17-11

--- wordpress/wp-content/plugins/meu_crud/lista_tpl.php
+++ wordpress/wp-content/plugins/meu_crud/lista_tpl.php
@@ -5,38 +5,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Meu Crud</title>
 </head>
-<body>
+<body class="wrap">
     <h1>Meu CRUD</h1>
     <br><br>
 
-<form method="post">
+    <form method="post">
 
-    <table border="1" width="50%">
-    <tr>
-        <td>Nome</td>
-        <td>Whatsapp</td>
-        <td></td>
-
-        <?php
-        
-            foreach($contatos as $key => $value){
-
-                echo "<tr>
-                        <td>{$value->nome}</td>
-                        <td>{$value->whatsapp}</td>
-                        <td><a href='?page={$_GET['page']}&apagar={value->id}'>Apagar</a></td>
-                        </tr>";
-            }
-        
-        ?>
+        <table border="1" width="50%">
         <tr>
-            <td><input type="text" name="nome"></td>
-            <td><input type="text" name="whatsapp"></td>
-            <td><?php submit_button('Gravar'); ?></td>
-            
+            <td>Nome</td>
+            <td>Whatsapp</td>
+            <td></td>
         </tr>
-    </tr>
-    </table>
+            <?php
+            
+                foreach($contatos as $key => $value){
+
+                    echo "<tr>
+                            <td>{$value->nome}</td>
+                            <td>{$value->whatsapp}</td>
+                            <td><a href='?page={$_GET['page']}&apagar={$value->id}'>Apagar</a></td>
+                            </tr>";
+                }
+            
+            ?>
+            <tr>
+                <td><input type="text" name="nome" placeholder="Nome"></td>
+                <td><input type="text" name="whatsapp" placeholder="Whatsapp"></td>
+                <td><?php submit_button('Gravar'); ?></td>
+                
+            </tr>
+        </tr>
+        </table>
 
     </form>
 
--- wordpress/wp-content/plugins/meu_crud/meu_crud.php
+++ wordpress/wp-content/plugins/meu_crud/meu_crud.php
@@ -26,3 +26,57 @@ function criar_tabela(){
                 whatsapp BIGINT UNSIGNED NOT NULL)");
 
 }
+
+register_deactivation_hook(__FILE__, 'apagar_tabela');
+
+function apagar_tabela(){
+    
+    global $wpdb;
+    
+    $wpdb->query("DROP TABLE {$wpdb->prefix}agenda");
+}
+
+add_action('admin_menu','crud_do_meu_plugin');
+
+
+ function crud_do_meu_plugin(){
+
+    //EXEMPLO PARA CRIAR ITEM NO PRIMEIRO NIVEL DO MENU
+     add_menu_page('configurações do Meu Plug-in', 
+                    'Meu CRUD', //NOME DO PLUGIN
+                    'administrator', 
+                    'meu-plugin-config',
+                    'abre_configuracoes',
+                    'dashicons-database');
+}
+
+function abre_configuracoes(){
+
+    global $wpdb;
+
+    // apagar
+    if(isset($_GET['apagar'])){
+
+        $id = preg_replace('/\D/', '', $_GET['apagar']); // pega os dados confiaveis
+
+        $wpdb->query("DELETE FROM {$wpdb->prefix}agenda WHERE id = $id");
+
+    }
+
+    // gravar
+    if(isset($_POST['submit'])){
+        
+        if($_POST['submit']=='Gravar'){
+
+            $wpdb->query( $wpdb->prepare("  INSERT INTO {$wpdb->prefix}agenda 
+                                                (nome, whatsapp)
+                                            VALUES
+                                                (%s, %d)",$_POST['nome'], $_POST['whatsapp'] ));
+        }
+    }
+
+    // listar
+    $contatos = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}agenda");
+
+    require 'lista_tpl.php';
+}
